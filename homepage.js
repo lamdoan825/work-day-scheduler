ba706869dcc7b0c5110425e9902f923a70cbbd82
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-04-25 21:11:06 -0500

    added text area and date and time

--- homepage.js
+++ homepage.js
@@ -1,9 +1,24 @@
 // Display date and time
+let todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
 // Use moment to for time format
 
 // Use JQuery to grab values of description
 
-// Create an if statement to check time for background color
+// Save text in local storage
 
-// Use localstorage to save task
+    // Use localstorage to get item
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+ 
+    // Create an if statement to check time for background color
+ 
