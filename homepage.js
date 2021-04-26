b8f2f8ba2984146da7a2399fde6da5d8e9c52ac3
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-04-25 20:44:18 -0500

    added the timeblock structure and padded pseudocode in js

---
+++ homepage.js
@@ -0,0 +1,9 @@
+// Display date and time
+
+// Use moment to for time format
+
+// Use JQuery to grab values of description
+
+// Create an if statement to check time for background color
+
+// Use localstorage to save task
