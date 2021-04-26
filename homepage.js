de26fbac697de5001dae76f6ccd6ef50bc1af21b
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-04-25 22:08:51 -0500

    created if statement for past present future

--- homepage.js
+++ homepage.js
@@ -26,16 +26,31 @@ $(document).ready(function () {
   $("#hour17 .description").val(localStorage.getItem("hour17"));
   
   function timeTracker() {
-    // get current number of hours.
+    // get current hour
     var timeNow = moment().hour();
 
     // time block loop
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      // To check the time and hopefully changed background colors
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
     })
-    
-  }  
-  timeTracker();
-    // Create an if statement to check time for background color
-    
+  }
+
+  timeTracker();   
 })
